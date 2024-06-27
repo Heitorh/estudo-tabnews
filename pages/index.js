748ae258e8eafd48b5dc39403e0bbb8922aff17c
Heitor <heitor.hino@gmail.com>
2024-06-27 20:20:23 +0000

    adicionando emoji

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Oi Kelly, Yuji e Roy, eu amo muito todos vocês</h1>
+    return <h1>❤️ Oi Kelly, Yuji e Roy, eu amo muito todos vocês ❤️</h1>
 }
 
 export default Home;
